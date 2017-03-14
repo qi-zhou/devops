2bccd8c2f51e20396413cfc8ae984dede583925e
Zhou Qi <zqbx81@gmail.com>
2017-03-14 22:04:31 +0800

    tmp

--- devops/urls.py
+++ devops/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+import xadmin
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'devops.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(xadmin.site.urls)),
 )
--- navigation/views.py
+++ navigation/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from navigation.models import *
 
 # Create your views here.
+def index(request):
+    gonggao = notice.objects.all().order_by('-date')[0]
+    fenlei = classify.objects.all().order_by('sort')
+    return render(request, 'index.html', {'fenlei': fenlei, 'index': 'index', 'gongao': gonggao})
